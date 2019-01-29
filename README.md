164a34d1b4821b129c9691c53fe5fa8b28b0b6b9
Mark Dicker <mark@markdicker.co.uk>
2019-01-29 10:35:40 +0000

    update

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 
 ### Status : Mothballed
 
+© Mark Dicker
+
--- build.bat
+++
@@ -1,9 +0,0 @@
-
-md c:\www\vandergraaf.local\wordpress\wp-content\plugins\vandergraaf
-md c:\www\vandergraaf.local\wordpress\wp-content\plugins\vandergraaf\classes
-md c:\www\vandergraaf.local\wordpress\wp-content\plugins\vandergraaf\js
-
-copy vandergraaf.php c:\www\vandergraaf.local\wordpress\wp-content\plugins\vandergraaf
-copy vandergraaf-logo.png c:\www\vandergraaf.local\wordpress\wp-content\plugins\vandergraaf
-copy classes\*.* c:\www\vandergraaf.local\wordpress\wp-content\plugins\vandergraaf\classes
-copy js\vandergraaf.js c:\www\vandergraaf.local\wordpress\wp-content\plugins\vandergraaf\js\vandergraaf.js
